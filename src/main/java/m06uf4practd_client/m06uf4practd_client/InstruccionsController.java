516c2df031ee8a66a6d5202be85ab9383180d5f8
Txell Llanas <>
2023-05-16 17:14:39 +0200

    20230516 -> Afegir vista Instruccions joc

---
+++ src/main/java/m06uf4practd_client/m06uf4practd_client/InstruccionsController.java
@@ -0,0 +1,34 @@
+package m06uf4practd_client.m06uf4practd_client;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
+/**
+ * Controlador de la vista 'Instruccions'. Aquesta vista mostra un texte d'ajuda
+ * que mostra les normes del joc.
+ *
+ * @author Txell Llanas
+ */
+public class InstruccionsController implements Initializable {
+
+    @FXML
+    private TextArea instruccionsJoc;
+    @FXML
+    private Label descripcioVerd;
+    @FXML
+    private Label descripcioVerd1;
+    @FXML
+    private Label descripcioVerd2;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    
+    
+}
